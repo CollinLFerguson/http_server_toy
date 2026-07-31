011a120f899de13aabc84a53ca7a8aa3b19268e4
Collin Ferguson <Collinleeferguson@gmail.com>
2026-07-31 18:59:35 -0400

    adds socket creation and binding

--- src/main.c
+++ src/main.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 #include <include/connection.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main(){
+#include <errno.h>
+
+#define _DEFAULT_PORT 3000
+
+int main(void){
     printf("Hello World!\n");
-    int foo = 4;
-    int bar = 2;
-    int flump = foo/bar;
-    int bungus = test();
+    
+    struct sockaddr_in socketaddr = 
+    {
+        .sin_family = AF_INET,
+        .sin_port = htons(_DEFAULT_PORT),
+        .sin_addr.s_addr = INADDR_ANY
+    };
 
-    printf("%d\n", flump);
-    printf("%d\n", bungus);
+    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(socketfd == -1){
+        //further error handling here
+        printf("Failure to acquire socket. Error: %d", errno);
+        return -1;
+    }
+    int bind_res = bind(socketfd, (struct socketaddr *) &socketaddr, sizeof(socketaddr));
+    
+    if(bind_res == -1){
+        printf("Failure to bind socket. Error: %d", errno);
+        return -1;
+    }
+
+    //listen(socket)
+    
+    shutdown(socketfd, SHUT_RDWR);
     return 0;
 }
