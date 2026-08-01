914a233031701b7464fd3217662916689314b491
Collin Ferguson <collinleeferguson@gmail.com>
2026-08-01 18:44:16 -0400

    sockets will correctly bind, listen, and accept a single connection before closing.

--- src/main.c
+++ src/main.c
@@ -10,8 +10,9 @@
 int main(void){
     printf("Hello World!\n");
     
-    struct sockaddr_in socketaddr = 
-    {
+    struct sockaddr_in peer_addr;
+
+    struct sockaddr_in host_addr = {
         .sin_family = AF_INET,
         .sin_port = htons(_DEFAULT_PORT),
         .sin_addr.s_addr = INADDR_ANY
@@ -23,15 +24,32 @@ int main(void){
         printf("Failure to acquire socket. Error: %d", errno);
         return -1;
     }
-    int bind_res = bind(socketfd, (struct socketaddr *) &socketaddr, sizeof(socketaddr));
+    int bind_res = bind(socketfd, (struct sockaddr *) &host_addr, sizeof(struct sockaddr));
     
     if(bind_res == -1){
         printf("Failure to bind socket. Error: %d", errno);
         return -1;
     }
 
-    //listen(socket)
+    int listen_res = listen(socketfd, 128);
     
+    if(listen_res == -1) {
+        printf("Failure to begin listening for connection. Error: %d", errno);
+        return -1;
+    }
+    
+    printf("Started listening for oncoming connection on port: %d\n", _DEFAULT_PORT);
+
+    socklen_t peer_addr_size = sizeof(&peer_addr);
+
+    int connection_descriptor = accept(socketfd, (struct sockaddr *) &peer_addr, &peer_addr_size);
+    if(connection_descriptor == -1) {
+        printf("Failure to accept oncoming connection. Error: %d", errno);
+        return -1;
+    }
+
+    printf("Client connected successfully");
+
     shutdown(socketfd, SHUT_RDWR);
     return 0;
 }
